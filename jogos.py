752a6b9c6e543522892d0b130f8dc23ee35d1cbd
VictorHugoBTaborda <victor.biscaia.taborda@escola.pr.gov.br>
2026-03-12 08:24:46 -0300

    jogos.py

--- jogos.py
+++ jogos.py
@@ -1,21 +1,41 @@
-import forca
-import adivinhacao
-
-def escolhe_jogo():
+def jogar():
     print("*********************************")
-    print("*******Escolha o seu jogo!*******")
+    print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    print("(1) Forca (2) Adivinhação")
+    palavra_secreta = "maça".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
-    jogo = int(input("Qual jogo? "))
+    enforcou = False
+    acertou = False
+    erros = 0
 
-    if(jogo == 1):
-        print("Jogando forca")
-        forca.jogar()
-    elif(jogo == 2):
-        print("Jogando adivinhação")
-        adivinhacao.jogar()
+    print(letras_acertadas)
+
+    while(not enforcou and not acertou):
+
+        chute = input("Qual letra? ")
+        chute = chute.strip().upper()
+
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+
+
+    if(acertou):
+        print("Você ganhou!!")
+    else:
+        print("Você perdeu!!")
+    print("Fim do jogo")
 
 if(__name__ == "__main__"):
-    escolhe_jogo()
+    jogar()
